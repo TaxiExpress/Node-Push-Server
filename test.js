b908de3b6b3ffbd04c119bec5168488397d010dd
MikelDTB <mikeldeltio90@gmail.com>
2014-01-23 20:14:24 +0100

    sendSelectedTaxi

--- test.js
+++ test.js
@@ -24,6 +24,13 @@ pushId4:''
 	console.log (new Date().toJSON().slice(0,10) + '  ' + new Date().toLocaleTimeString()  + '  POST: /sendClosestTaxi ' + res.statusCode);
 });
 
+client.post('/sendSelectedTaxi', { origin: 'world',  startpoint:'Pues que bien', travelID: 'android',valuation:'Pues que bien', device: 'android',
+pushId0:'APA91bHJRkpSjXvlFA7L94ybyalAeW0BxE0Z1K4g99onHvXLIFgptSJDhBIMXckY9HBzaBpEWo4Se9zUCd2KjzWUHCJ5TLac-qF-Hu8ozi7Uoe14ZFRg2_c82xmL4ZXgMfuhec4UUd-eu_SkYsMPRt2bqNZ0K5Uzgpwd2en9454w8-f3c7pyEK0'
+ }, function(err, req, res, data) {
+	assert.ifError(err); 
+	console.log (new Date().toJSON().slice(0,10) + '  ' + new Date().toLocaleTimeString()  + '  POST: /sendClosestTaxi ' + res.statusCode);
+});
+
 client.post('/sendAcceptTravel', { latitude: '2125',  longitude:'4556', travelID: '01', device: 'android',
 pushId0:'APA91bHJRkpSjXvlFA7L94ybyalAeW0BxE0Z1K4g99onHvXLIFgptSJDhBIMXckY9HBzaBpEWo4Se9zUCd2KjzWUHCJ5TLac-qF-Hu8ozi7Uoe14ZFRg2_c82xmL4ZXgMfuhec4UUd-eu_SkYsMPRt2bqNZ0K5Uzgpwd2en9454w8-f3c7pyEK0'
  }, function(err, req, res, data) {
