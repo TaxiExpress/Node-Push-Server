00f52827e37c1e4221b0d4781667172621bcea60
MikelDTB <mikeldeltio90@gmail.com>
2015-02-10 20:13:31 +0100

    Update test-android.js

--- test-android.js
+++ test-android.js
@@ -2,10 +2,9 @@ var assert = require('assert');
 var restify = require('restify');
 
 var client = restify.createJsonClient({
-	url: 'http://127.6.39.129:8080',
+	url: process.env.OPENSHIFT_NODEJS_IP + ':' + parseInt(process.env.OPENSHIFT_NODEJS_PORT),
 	version: '0.0.1'
 });
-
 
 client.post('/push', { origin: 'world',  startpoint:'Pues que bien', travelID: 'android',valuation:'Pues que bien', device: 'ANDROID', phone: '666666666',
 pushId:'APA91bHdfAsMRF1C3YXJhv0AGOSUFN8tr66zue3J6HdVtUlcZYk9OAoix2ZNzHuIKZ9khVxKvxRR25OTwnFKu9WlACi8IvnPaD4qfts8Jjih4259AoR0u52HdaMLhkBq4NCpDcOZl5a2RJYAuQaFs9Gl8FwTtrodo2jdSdoVItbYaIixV2cfKXI',
